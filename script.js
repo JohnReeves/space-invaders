10516a4ed1e8fcb9d2e8eb94e085b54d01a5f491
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2020-01-29 12:40:02 +0000

    transform function

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 var row1 = $("#row1");
 
-var elTranform = {
-    rot: 0,   // the rotation 'counter' for the element 'el'
-    sca: 1,   // the scale 'counter' for the element 'el'
+//row1.attr("style","transform: rotate(30deg) skew(45deg)");
+
+var elTransform = {
+    rot: 30,   // the rotation 'counter' for the element 'el'
+    sca: 10,   // the scale 'counter' for the element 'el'
     skx: 0,   // the skewX 'counter' for the element 'el'
     sky: 0    // the skewY 'counter' for the element 'el'
 };
 
-console.log(elTranform);
+console.log(elTransform);
+
 setTransform(row1, elTransform);
 
 function setTransform (element, elTransformArg) {
-    var transfromString = ("rotate(" + elTransformArg.rot + "deg ) scale("
+  // element.style.transform = ("rotate() scale() skewX() skewY()");
+    var transfromString = ("rotate(" + elTransformArg.rot + "deg) scale("
         + elTransformArg.sca + ") skewX(" + elTransformArg.skx + "deg ) skewY("
         + elTransformArg.sky + "deg )");
 
 console.log(element);
+console.log(transfromString);
 
     // now attach that variable to each prefixed style
-    element.style.webkitTransform = transfromString;
-    element.style.MozTransform = transfromString;
-    element.style.msTransform = transfromString;
-    element.style.OTransform = transfromString;
-    element.style.transform = transfromString;
+    // element.style.webkitTransform = transfromString;
+    //element.style.MozTransform = transfromString;
+    //element.style.msTransform = transfromString;
+    //element.style.OTransform = transfromString;
+    //element.style.transform = (transfromString);
+    element.attr("style","transform: rotate(30deg) skew(45deg)");
+
+
 }
 
