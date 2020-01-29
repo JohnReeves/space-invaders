5aff096810b550ab72b075c00182f15b9b7aa304
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2020-01-29 13:08:55 +0000

    space invaders movement-ish

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
 var row1 = $("#row1");
+var count = 0;
+var row1Trans = {dX: 1, dY: 0};
 
-//row1.attr("style","transform: rotate(30deg) skew(45deg)");
+function moveRow(){
+  count++;
+  console.log(count);
+  if (count%10 == 0) {
+    row1Trans.dY += 10;
+    row1Trans.dX -= 5;
+  }
+  else 
+    row1Trans.dX += 5;
 
-var elTransform = {
-    rot: 30,   // the rotation 'counter' for the element 'el'
-    sca: 10,   // the scale 'counter' for the element 'el'
-    skx: 0,   // the skewX 'counter' for the element 'el'
-    sky: 0    // the skewY 'counter' for the element 'el'
-};
-
-console.log(elTransform);
-
-setTransform(row1, elTransform);
-
-function setTransform (element, elTransformArg) {
-  // element.style.transform = ("rotate() scale() skewX() skewY()");
-    var transfromString = ("rotate(" + elTransformArg.rot + "deg) scale("
-        + elTransformArg.sca + ") skewX(" + elTransformArg.skx + "deg ) skewY("
-        + elTransformArg.sky + "deg )");
-
-console.log(element);
-console.log(transfromString);
-
-    // now attach that variable to each prefixed style
-    // element.style.webkitTransform = transfromString;
-    //element.style.MozTransform = transfromString;
-    //element.style.msTransform = transfromString;
-    //element.style.OTransform = transfromString;
-    //element.style.transform = (transfromString);
-    element.attr("style","transform: rotate(30deg) skew(45deg)");
-
-
+  setTransform(row1, row1Trans);
 }
 
+function setTransform (svg, svgTran) {
+    var svgTranString = 
+    ("translatex("+svgTran.dX+"px) "+
+     "translatey("+svgTran.dY+ "px)");
+
+    svg.attr("style","transform: "+svgTranString);
+}
+
+setInterval(moveRow,500);
